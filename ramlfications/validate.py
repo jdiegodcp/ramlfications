229474f92c5a4bbfa9fafb6a8836512d238eb104
Lynn Root <lynn@spotify.com>
2014-09-30 09:16:30 -0700

    Initial validate.py setup

--- ramlfications/validate.py
+++ ramlfications/validate.py
@@ -3,26 +3,30 @@
 # Copyright (c) 2014 Spotify AB
 
 
+from .parser import APIRoot
+
+
 VALID_RAML_VERSIONS = ["0.8"]
 
 
-class ValidateRAMLError(Exception):
+class RAMLValidationError(Exception):
     pass
 
 
 class ValidateRAML(object):
     def __init__(self, raml_file):
-        self.raml = raml_file
+        self.raml_file = raml_file
+        self.api = APIRoot(raml_file)
 
-    def validate_raml_header(self):
-        with open(self.raml, 'r') as r:
+    def raml_header(self):
+        with open(self.raml_file, 'r') as r:
             raml_header = r.readline().split('\n')[0]
             raml_def, version = raml_header.split()
             if raml_def != "#%RAML":
                 msg = "Not a valid RAML header: {0}.".format(raml_def)
-                raise ValidateRAMLError(msg)
+                raise RAMLValidationError(msg)
             if version not in VALID_RAML_VERSIONS:
                 msg = "Not a valid version of RAML: {0}.".format(version)
-                raise ValidateRAMLError(msg)
+                raise RAMLValidationError(msg)
 
             return raml_def, version
