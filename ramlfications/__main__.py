664fadf6cb4f432b98e489796b8dc02adaab0739
Lynn Root <lynn@spotify.com>
2014-10-22 15:38:12 -0400

    Initial workings of entry points

--- ramlfications/__main__.py
+++ ramlfications/__main__.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2014 Spotify AB
 
+from parser import APIRoot
+from validate import ValidateRAML, RAMLValidationError
 
 import click
 
@@ -13,21 +15,27 @@ def main():
     """The main routine."""
 
 
-@main.command()
+@main.command(help="Parse a RAML file")
 @click.argument('ramlfile', type=click.Path(exists=True))
 def parse(ramlfile):
-    click.echo("Parsing file: {0}".format(ramlfile))
-    print 'parse: {0}'.format(ramlfile)
-    click.echo("Parsed file: {0}".format(ramlfile))
+    click.echo("- Parsing file: {0}".format(ramlfile))
+    api = APIRoot(ramlfile)
+    print '✔︎ Loaded: {0}'.format(api.title)
+    nodes = api.nodes
+    print "✔︎ Parsed {0} nodes.".format(len(nodes.values()))
+    click.echo("✔︎ Parsed file: {0}".format(ramlfile))
 
 
-@main.command()
+@main.command(help="Validate a RAML file.")
 @click.argument('ramlfile', type=click.Path(exists=True))
 def validate(ramlfile):
-    click.echo("Validating file: {0}".format(ramlfile))
-    print 'validate: {0}'.format(ramlfile)
-    click.echo("Validated file: {0}".format(ramlfile))
+    click.echo("- Validating file: {0}".format(ramlfile))
 
+    try:
+        ValidateRAML(ramlfile).validate()
+        click.echo("✔︎ Validated file: {0}".format(ramlfile))
+    except RAMLValidationError as e:
+        click.echo("✗ Error validating file {0}: {1}".format(ramlfile, e))
 
 @main.command()
 @click.argument('ramlfile', type=click.Path(exists=True))
