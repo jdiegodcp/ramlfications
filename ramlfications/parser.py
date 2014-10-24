81617ca220fbef74b66ec0929e6293eb23cc10c8
Lynn Root <lynn@spotify.com>
2014-10-24 18:09:09 -0400

    Switched to importing HTTP response codes
    rather than typing them all out.

--- ramlfications/parser.py
+++ ramlfications/parser.py
@@ -9,6 +9,7 @@ except ImportError:  # pragma: no cover
     # python 2.6
     from ordereddict import OrderedDict
 
+import BaseHTTPServer
 import json
 import re
 
@@ -20,19 +21,11 @@ from .parameters import (ContentType, FormParameter, URIParameter,
                          Documentation, SecuritySchemes, Body)
 
 
+HTTP_RESP_CODES = BaseHTTPServer.BaseHTTPRequestHandler.responses.keys()
+
+
 class RAMLParserError(Exception):
     pass
-
-
-HTTP_RESP_CODES = [100, 101, 102,
-                   200, 201, 202, 203, 204, 205, 206, 207, 208, 226,
-                   300, 301, 302, 303, 304, 305, 306, 307, 308,
-                   400, 401, 402, 403, 404, 405, 406, 407, 408, 409,
-                   410, 411, 412, 413, 414, 415, 416, 417, 418, 420,
-                   422, 423, 424, 425, 426, 428, 429, 431, 444, 449,
-                   450, 499,
-                   500, 501, 502, 503, 504, 505, 509, 510, 511, 598,
-                   599]
 
 
 class APIRoot(object):
