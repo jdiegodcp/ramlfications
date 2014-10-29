0c43a1a429d6ab6e03e7d277a6f6fe6cc0ab7650
Lynn Root <lynn@spotify.com>
2014-10-29 10:22:39 -0700

    removed unneeded 'v' in version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ramlfications",
-    version="v0.1.0",
+    version="0.1.0",
     description="Python RAML parser",
     long_description=("TODO"),
     url="https://ramlfications.readthedocs.org/",
