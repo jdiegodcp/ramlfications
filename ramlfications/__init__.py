c016621e291081c6581c000b2a8e4d8de36e1ece
Lynn Root <lynn@spotify.com>
2014-10-30 20:58:05 -0700

    added a load helper function

--- ramlfications/__init__.py
+++ ramlfications/__init__.py
@@ -9,10 +9,12 @@ __version__ = '0.1.0'
 __license__ = 'Apache 2.0'
 
 
+from ramlfications.loader import load
 from ramlfications.parser import parse
 from ramlfications.validate import validate
 
 __all__ = [
+    'load',
     'parse',
     'validate'
 ]
