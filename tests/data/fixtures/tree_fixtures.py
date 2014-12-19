5f67f38a6d5b91b2af3afca1ac1c660280e0739c
Lynn Root <lynn@spotify.com>
2014-12-19 15:20:41 -0800

    FIX: consistent pipe coloring

--- tests/data/fixtures/tree_fixtures.py
+++ tests/data/fixtures/tree_fixtures.py
@@ -19,26 +19,43 @@ Base URI: https://api.spotify.com/v1
 |  - /users/{user_id}/playlists/{playlist_id}
 """)
 
+# NOTE: Since I am having the pipe ("|") consistent throughout in terms
+# of color, as well as setting its ANSI color separately from the rest
+# of the string - even if they are the same color - tests need to have
+# call `colored()` around the pipe separately.
+
 tree_light = (
     colored("==================================", "white") + "\n" +
     colored("Spotify Web API Demo - Simple Tree", "yellow") + "\n" +
     colored("==================================", "white") + "\n" +
     colored("Base URI: https://api.spotify.com/v1", "yellow") + "\n" +
-    colored("|- /tracks", "green", attrs=["bold"]) + "\n" +
-    colored("|  - /tracks/{id}", "green", attrs=["bold"]) + "\n" +
-    colored("|- /users/{user_id}/playlists", "green", attrs=["bold"]) + "\n" +
-    colored("|  - /users/{user_id}/playlists/{playlist_id}", "green", attrs=["bold"]) + "\n"
+    colored("|", "green", attrs=["bold"]) +
+    colored("- /tracks", "green", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("  - /tracks/{id}", "green", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("- /users/{user_id}/playlists", "green", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("  - /users/{user_id}/playlists/{playlist_id}", "green",
+            attrs=["bold"]) + "\n"
 )
 
 tree_dark = (
     colored("==================================", "grey") + "\n" +
-    colored("Spotify Web API Demo - Simple Tree", "grey", attrs=["bold"]) + "\n" +
+    colored("Spotify Web API Demo - Simple Tree", "grey",
+            attrs=["bold"]) + "\n" +
     colored("==================================", "grey") + "\n" +
-    colored("Base URI: https://api.spotify.com/v1", "grey", attrs=["bold"]) + "\n" +
-    colored("|- /tracks", "grey", attrs=["bold"]) + "\n" +
-    colored("|  - /tracks/{id}", "grey", attrs=["bold"]) + "\n" +
-    colored("|- /users/{user_id}/playlists", "grey", attrs=["bold"]) + "\n" +
-    colored("|  - /users/{user_id}/playlists/{playlist_id}", "grey", attrs=["bold"]) + "\n"
+    colored("Base URI: https://api.spotify.com/v1",
+            "grey", attrs=["bold"]) + "\n" +
+    colored("|", "grey", attrs=["bold"]) +
+    colored("- /tracks", "grey", attrs=["bold"]) + "\n" +
+    colored("|", "grey", attrs=["bold"]) +
+    colored("  - /tracks/{id}", "grey", attrs=["bold"]) + "\n" +
+    colored("|", "grey", attrs=["bold"]) +
+    colored("- /users/{user_id}/playlists", "grey", attrs=["bold"]) + "\n" +
+    colored("|", "grey", attrs=["bold"]) +
+    colored("  - /users/{user_id}/playlists/{playlist_id}", "grey",
+            attrs=["bold"]) + "\n"
 )
 
 tree_light_v = (
@@ -46,14 +63,24 @@ tree_light_v = (
     colored("Spotify Web API Demo - Simple Tree", "yellow") + "\n" +
     colored("==================================", "white") + "\n" +
     colored("Base URI: https://api.spotify.com/v1", "yellow") + "\n" +
-    colored("|- /tracks", "green", attrs=["bold"]) + "\n" +
-    colored("|  ⌙ GET", "blue", attrs=["bold"]) + "\n" +
-    colored("|  - /tracks/{id}", "green", attrs=["bold"]) + "\n" +
-    colored("|    ⌙ GET", "blue", attrs=["bold"]) + "\n" +
-    colored("|- /users/{user_id}/playlists", "green", attrs=["bold"]) + "\n" +
-    colored("|  ⌙ GET", "blue", attrs=["bold"]) + "\n" +
-    colored("|  - /users/{user_id}/playlists/{playlist_id}", "green", attrs=["bold"]) + "\n" +
-    colored("|    ⌙ PUT", "blue", attrs=["bold"]) + "\n"
+    colored("|", "green", attrs=["bold"]) +
+    colored("- /tracks", "green", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("  ⌙ GET", "blue", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("  - /tracks/{id}", "green", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("    ⌙ GET", "blue", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("- /users/{user_id}/playlists", "green",
+            attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("  ⌙ GET", "blue", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("  - /users/{user_id}/playlists/{playlist_id}", "green",
+            attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("    ⌙ PUT", "blue", attrs=["bold"]) + "\n"
 )
 
 tree_light_vv = (
@@ -61,24 +88,43 @@ tree_light_vv = (
     colored("Spotify Web API Demo - Simple Tree", "yellow") + "\n" +
     colored("==================================", "white") + "\n" +
     colored("Base URI: https://api.spotify.com/v1", "yellow") + "\n" +
-    colored("|- /tracks", "green", attrs=["bold"]) + "\n" +
-    colored("|  ⌙ GET", "blue", attrs=["bold"]) + "\n" +
-    colored("|     Query Params", "cyan", attrs=["bold"]) + "\n" +
-    colored("|      ⌙ ids", "cyan", attrs=["bold"]) + "\n" +
-    colored("|  - /tracks/{id}", "green", attrs=["bold"]) + "\n" +
-    colored("|    ⌙ GET", "blue", attrs=["bold"]) + "\n" +
-    colored("|       URI Params", "cyan", attrs=["bold"]) + "\n" +
-    colored("|        ⌙ id", "cyan", attrs=["bold"]) + "\n" +
-    colored("|- /users/{user_id}/playlists", "green", attrs=["bold"]) + "\n" +
-    colored("|  ⌙ GET", "blue", attrs=["bold"]) + "\n" +
-    colored("|     URI Params", "cyan", attrs=["bold"]) + "\n" +
-    colored("|      ⌙ user_id", "cyan", attrs=["bold"]) + "\n" +
-    colored("|  - /users/{user_id}/playlists/{playlist_id}", "green", attrs=["bold"]) + "\n" +
-    colored("|    ⌙ PUT", "blue", attrs=["bold"]) + "\n" +
-    colored("|       URI Params", "cyan", attrs=["bold"]) + "\n" +
-    colored("|        ⌙ user_id", "cyan", attrs=["bold"]) + "\n" +
-    colored("|       Form Params", "cyan", attrs=["bold"]) + "\n" +
-    colored("|        ⌙ name", "cyan", attrs=["bold"]) + "\n"
+    colored("|", "green", attrs=["bold"]) +
+    colored("- /tracks", "green", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("  ⌙ GET", "blue", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("     Query Params", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("      ⌙ ids", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("  - /tracks/{id}", "green", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("    ⌙ GET", "blue", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("       URI Params", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("        ⌙ id", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("- /users/{user_id}/playlists", "green", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("  ⌙ GET", "blue", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("     URI Params", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("      ⌙ user_id", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("  - /users/{user_id}/playlists/{playlist_id}", "green",
+            attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("    ⌙ PUT", "blue", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("       URI Params", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("        ⌙ user_id", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("       Form Params", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("        ⌙ name", "cyan", attrs=["bold"]) + "\n"
 )
 
 tree_light_vvv = (
@@ -86,22 +132,41 @@ tree_light_vvv = (
     colored("Spotify Web API Demo - Simple Tree", "yellow") + "\n" +
     colored("==================================", "white") + "\n" +
     colored("Base URI: https://api.spotify.com/v1", "yellow") + "\n" +
-    colored("|- /tracks", "green", attrs=["bold"]) + "\n" +
-    colored("|  ⌙ GET", "blue", attrs=["bold"]) + "\n" +
-    colored("|     Query Params", "cyan", attrs=["bold"]) + "\n" +
-    colored("|      ⌙ ids: Spotify Track IDs", "cyan", attrs=["bold"]) + "\n" +
-    colored("|  - /tracks/{id}", "green", attrs=["bold"]) + "\n" +
-    colored("|    ⌙ GET", "blue", attrs=["bold"]) + "\n" +
-    colored("|       URI Params", "cyan", attrs=["bold"]) + "\n" +
-    colored("|        ⌙ id: Spotify Track ID", "cyan", attrs=["bold"]) + "\n" +
-    colored("|- /users/{user_id}/playlists", "green", attrs=["bold"]) + "\n" +
-    colored("|  ⌙ GET", "blue", attrs=["bold"]) + "\n" +
-    colored("|     URI Params", "cyan", attrs=["bold"]) + "\n" +
-    colored("|      ⌙ user_id: User ID", "cyan", attrs=["bold"]) + "\n" +
-    colored("|  - /users/{user_id}/playlists/{playlist_id}", "green", attrs=["bold"]) + "\n" +
-    colored("|    ⌙ PUT", "blue", attrs=["bold"]) + "\n" +
-    colored("|       URI Params", "cyan", attrs=["bold"]) + "\n" +
-    colored("|        ⌙ user_id: User ID", "cyan", attrs=["bold"]) + "\n" +
-    colored("|       Form Params", "cyan", attrs=["bold"]) + "\n" +
-    colored("|        ⌙ name: Playlist Name", "cyan", attrs=["bold"]) + "\n"
+    colored("|", "green", attrs=["bold"]) +
+    colored("- /tracks", "green", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("  ⌙ GET", "blue", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("     Query Params", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("      ⌙ ids: Spotify Track IDs", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("  - /tracks/{id}", "green", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("    ⌙ GET", "blue", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("       URI Params", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("        ⌙ id: Spotify Track ID", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("- /users/{user_id}/playlists", "green", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("  ⌙ GET", "blue", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("     URI Params", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("      ⌙ user_id: User ID", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("  - /users/{user_id}/playlists/{playlist_id}", "green",
+            attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("    ⌙ PUT", "blue", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("       URI Params", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("        ⌙ user_id: User ID", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("       Form Params", "cyan", attrs=["bold"]) + "\n" +
+    colored("|", "green", attrs=["bold"]) +
+    colored("        ⌙ name: Playlist Name", "cyan", attrs=["bold"]) + "\n"
 )
