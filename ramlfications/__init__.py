2eeb2f656803269be67814d5639da1b368ee6456
Lynn Root <lynn@spotify.com>
2015-03-08 16:02:43 -0700

    Switch single to double quotes
    because I'm anal.

--- ramlfications/__init__.py
+++ ramlfications/__init__.py
@@ -4,9 +4,9 @@
 
 from __future__ import absolute_import, division, print_function
 
-__author__ = 'Lynn Root'
-__version__ = '0.1.0'
-__license__ = 'Apache 2.0'
+__author__ = "Lynn Root"
+__version__ = "0.1.0"
+__license__ = "Apache 2.0"
 
 import os
 
