d99c244a43fe45b9810899cddd82d4b584bf432f
Nick Villalva <nvillalva@underarmour.com>
2015-06-12 11:24:12 -0500

    load JSON $refs
    Supports the loading of JSON `$ref`s from URIs, absolute paths and
    relative paths.

--- ramlfications/loader.py
+++ ramlfications/loader.py
@@ -8,17 +8,22 @@ try:
 except ImportError:  # pragma: no cover
     from ordereddict import OrderedDict
 
+import copy
 import os
+import urlparse
 
 import yaml
 
 from .errors import LoadRAMLError
+from .utils import download_url
 
 
 class RAMLLoader(object):
     """
     Extends YAML loader to load RAML files with ``!include`` tags.
     """
+    refs = {}
+
     def _yaml_include(self, loader, node):
         """
         Adds the ability to follow ``!include`` directives within
@@ -35,7 +40,75 @@ class RAMLLoader(object):
                 return inputfile.read()
 
         with open(file_name) as inputfile:
-            return yaml.load(inputfile, self._ordered_loader)
+            parsed = yaml.load(inputfile, self._ordered_loader)
+            if file_ext == ".json":
+                parsed = self._parse_json_refs(
+                    parsed, os.path.dirname(file_name)
+                )
+            return parsed
+
+    def _lookup_json_ref(self, ref_key, base_path=None):
+        ref_source, ref_fragment = ref_key.split("#/")
+
+        # Determine the correct identifier
+        relative_path = os.path.join(base_path, ref_source)
+        file_name, uri = None, None
+        if ref_source.startswith('/'):  # Absolute path
+            file_name = ref_source if os.path.isfile(ref_source) else None
+        elif bool(urlparse.urlparse(ref_source).scheme):  # URL
+            uri = ref_source
+        elif base_path and os.path.exists(relative_path):
+            file_name = relative_path
+        else:
+            raise LoadRAMLError(
+                "Invalid JSON reference: unable to process %s (%s, %s)" %
+                (ref_source, file_name, uri)
+            )
+
+        # Load the ref if we dont have it
+        if ref_source not in self.refs.keys():
+            if file_name is not None:
+                with open(file_name) as reffed_file:
+                    self.refs[file_name] = yaml.load(
+                        reffed_file,
+                        self._ordered_loader
+                    )
+            elif uri is not None:
+                response = download_url(uri)
+                self.refs[uri] = yaml.load(response)
+
+        reffed_json = self.refs[file_name if file_name else uri]
+
+        # If no path, use the whole object, otherwise down the hole we go
+        for key in filter(lambda x: len(x) > 0, ref_fragment.split('/')):
+            try:
+                reffed_json = reffed_json[key]
+            except KeyError:
+                raise LoadRAMLError(
+                    "Invalid JSON reference: %s not found in %s" %
+                    (ref_fragment, reffed_json.keys())
+                )
+
+        return reffed_json
+
+    def _parse_json_refs(self, schema, base_path=None):
+        expanded = copy.deepcopy(schema)
+        if type(schema) in [dict, OrderedDict]:
+            for k, v in schema.items():
+                if type(v) in [dict, OrderedDict]:
+                    expanded[k] = self._parse_json_refs(v, base_path)
+                elif type(v) == list:
+                    for idx in xrange(len(v)):
+                        expanded[k][idx] = self._parse_json_refs(
+                            v[idx], base_path
+                        )
+                elif k == '$ref':
+                    # For a $ref, dereference it
+                    del expanded['$ref']
+                    expanded.update(
+                        self._lookup_json_ref(v, base_path=base_path)
+                    )
+        return expanded
 
     def _ordered_load(self, stream, loader=yaml.Loader):
         """
