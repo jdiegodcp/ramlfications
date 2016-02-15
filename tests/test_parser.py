4f9e4101bdf5b9f135f0d14d1baacc9bdbb6f0c0
Piët Delport <pjdelport@gmail.com>
2016-02-15 23:37:31 +0200

    Avoid hardcoding PYTHONHASHSEED in tests (squashed)
    3453b81 test_parser.py:
    
        Fix hash order dependency in test_resource_assigned_type
        This makes the test pass deterministically.
    
    9c57a40 test_parser.py:
    
        Fix hash order dependency in test_resource_inherits_get
    
        These two resources were previously listed in an unpredictable order,
        relative to each other: this way, the test does not depend on a
        particular order.
    
    fc1269d .travis.yml, tox.ini:
    
        Remove hash seed hard-coding from Tox and Travis configs
        The tests should now pass without this.
    
    4047bf7 AUTHORS.rst:
    
        Add myself to the contributor list

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -676,10 +676,11 @@ def test_resource_assigned_type(resources):
     assert res.headers[0] == res.resource_type.headers[0]
     assert res.body[0] == res.resource_type.body[0]
     assert res.responses[0] == res.resource_type.responses[0]
-    assert len(res.headers) == 3
-    assert res.headers[0].name == "X-another-header"
-    assert res.headers[1].name == "Accept"
-    assert res.headers[2].name == "X-example-header"
+    assert set([h.name for h in res.headers]) == set([
+        "X-another-header",
+        "Accept",
+        "X-example-header",
+    ])
 
     res = resources[18]
     assert res.type == "collection"
@@ -961,10 +962,13 @@ def test_resource_inherits_type_optional_get(inherited_resources):
 
 def test_resource_inherits_get(inherited_resources):
     assert len(inherited_resources.resources) == 7
-    post_res = inherited_resources.resources[3]
-    get_res = inherited_resources.resources[4]
 
-    assert get_res.method == "get"
+    rs = [r for r in inherited_resources.resources
+          if r.path == '/another-post-resource']
+    assert len(rs) == 2
+    [post_res] = [r for r in rs if r.method == 'post']
+    [get_res] = [r for r in rs if r.method == 'get']
+
     assert len(get_res.headers) == 1
     assert len(get_res.body) == 1
     assert len(get_res.responses) == 2
@@ -990,7 +994,6 @@ def test_resource_inherits_get(inherited_resources):
     assert q.max_length == 50
     assert q.required is True
 
-    assert post_res.method == "post"
     assert post_res.description.raw == "post some more foobar"
 
 
