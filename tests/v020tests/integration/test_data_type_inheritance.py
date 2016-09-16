ec19d7f0f17a711c33cea171efa972af86d5ce43
Lynn Root <lynn@lynnroot.com>
2016-09-16 13:38:30 -0400

    Add’l tests for data type inheritance

--- tests/v020tests/integration/test_data_type_inheritance.py
+++ tests/v020tests/integration/test_data_type_inheritance.py
@@ -14,7 +14,7 @@ from tests.base import RAMLEXAMPLES, V020EXAMPLES
 
 
 @pytest.fixture(scope="session")
-def api():
+def fragment_api():
     ramlfile = os.path.join(V020EXAMPLES, "uses_data_fragment.raml")
     loaded_raml = load_file(ramlfile)
     configfile = os.path.join(RAMLEXAMPLES, "simple_config.ini")
@@ -22,12 +22,12 @@ def api():
     return parse_raml(loaded_raml, config)
 
 
-def test_includes_fragment(api):
-    assert len(api.types) == 2
+def test_includes_fragment(fragment_api):
+    assert len(fragment_api.types) == 2
 
 
-def test_person_data_type(api):
-    person = api.types[0]
+def test_person_data_type(fragment_api):
+    person = fragment_api.types[0]
 
     assert person.type == 'object'
     assert person.name == 'Person'
@@ -38,8 +38,8 @@ def test_person_data_type(api):
     assert name.type == 'string'
 
 
-def test_employee_data_type(api):
-    employee = api.types[1]
+def test_employee_data_type(fragment_api):
+    employee = fragment_api.types[1]
 
     assert employee.type == 'Person'
     assert employee.name == 'Employee'
@@ -52,3 +52,28 @@ def test_employee_data_type(api):
     id_ = employee.properties.get('id')
     assert id_.required
     assert id_.type == 'string'
+
+
+@pytest.fixture(scope="session")
+def api():
+    ramlfile = os.path.join(V020EXAMPLES, "data_type_inheritance.raml")
+    loaded_raml = load_file(ramlfile)
+    configfile = os.path.join(RAMLEXAMPLES, "simple_config.ini")
+    config = setup_config(configfile)
+    return parse_raml(loaded_raml, config)
+
+
+def test_inherit_name(api):
+    person = api.types[0]
+    props = person.properties
+    first_name = props.get('firstName')
+    assert first_name.required
+    assert first_name.type == 'string'
+
+    last_name = props.get('lastName')
+    assert last_name.required
+    assert last_name.type == 'string'
+
+    middle_name = props.get('middleName')
+    assert not middle_name.required
+    assert middle_name.type == 'string'
